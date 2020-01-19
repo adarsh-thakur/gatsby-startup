64cc2a3cd0ebffa873a716c67095ca80d05271e7
adarsh_thakur <adarshthakur05@gmail.com>
2020-01-19 23:40:07 +0530

    Updates README

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../component/header'
+
+
+
+export default () =>(
+    <div>
+    <Header text="This is pass throgh the parent component"/>
+     <div>Above Header is a independent sub component</div>
+    <div>This is about page</div>
+    </div>
+
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import { Link } from "gatsby";
 
-export default () => <div>Hello world!</div>
+export default () => (
+<div>
+<h1>Hello Out there!</h1>
+<h3>Welcome to the gatsby-startup project</h3>
+<Link to="/about/">Go to about page</Link>
+</div>
+)
